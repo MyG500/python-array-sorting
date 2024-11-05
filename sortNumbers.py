2e7e52ec8376a97609c2405ccf37285941ef3550
MyG500 <111442725+MyG500@users.noreply.github.com>
2024-11-05 12:49:14 +0200

    Add files via upload

---
+++ sortNumbers.py
@@ -0,0 +1,11 @@
+# הגדרת המערך
+numbers = [4, 7, 1, 9, 4, 5]
+
+# הדפסת המערך לפני המיון
+print("Array before sorting:", numbers)
+
+# מיון המערך בסדר יורד
+numbers.sort(reverse=True)
+
+# הדפסת המערך אחרי המיון
+print("Array after sorting:", numbers)
